88286a74a884861574e9b4921dd6ab67c28abbfb
KavindaKiridana <kavindakiridena@gmail.com>
2025-05-20 19:54:22 +0530

    edit

--- password/Program.cs
+++ password/Program.cs
@@ -1,9 +1,8 @@
-﻿
-string password = "123";
-string hashedPassword=BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+﻿string password = "123";// Create a password
+string hashedPassword=BCrypt.Net.BCrypt.EnhancedHashPassword(password);// Hash the password
 Console.WriteLine(hashedPassword);
 
-bool verify=BCrypt.Net.BCrypt.EnhancedVerify(password,hashedPassword);
+bool verify=BCrypt.Net.BCrypt.EnhancedVerify(password,hashedPassword);// Verify the password matches its hash
 Console.WriteLine(verify);
 
 string savingPin = "$2a$11$gORzJST6d/2Nho6ayXL9lebybJebVbjsbrA5oL5eDumCKlob4XPV.";
